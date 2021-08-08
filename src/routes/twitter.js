7bd7d26eea2a83a7164e346a1ebfec2339eec930
James <85806019+James-Discord@users.noreply.github.com>
2021-08-08 10:45:18 +0100

    twitters test

---
+++ src/routes/twitter.js
@@ -0,0 +1,18 @@
+const { Canvas } = require('canvas-constructor')
+const canvas = require('canvas')
+module.exports = {
+  route: "/twitter",
+  run: async (req, res) => {
+    const img = await canvas.loadImage('https://teckspace.files.wordpress.com/2011/08/twitter1.jpg')
+
+    let image = new Canvas(550, 267)
+    .printImage(img, 0, 0, 550, 267)
+    .setTextFont('28px Impact')
+    .printText(req.params.feed, 40, 150)
+    .toBuffer();
+
+    res.set({'Content-Type': 'image/png'})//setting content type as png image!
+    res.send(image)//sending the image!
+    });
+  },
+};
